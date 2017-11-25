d20b5b9c48f9171e13c2df5464ea5792c49dd898
Natalia <natlaa95@gmail.com>
2017-11-25 08:33:50 +0100

    slider for start page

---
+++ EventApplication/EventApplication/Scripts/custom/customAccount.js
@@ -0,0 +1,27 @@
+﻿
+function CurrentSlide(n) {
+    ShowSlide(slideIndex = n);
+}
+
+function ShowSlide(n) {
+    var i;
+    var slides = document.getElementsByClassName("app_desc");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace("activeDot", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " activeDot";
+} 
+
+(function () {
+
+    $('#slide2').css("display", "block");
+    $('#slideDot2').addClass("activeDot");
+
+})();
